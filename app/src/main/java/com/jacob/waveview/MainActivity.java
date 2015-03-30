a6dcd22ce42c556af5c8963a8522152360a0b902
王佳 <wangjia@cvte.cn>
2015-03-30 17:29:05 +0800

    完成了具有波浪效果的控件

--- app/src/main/java/com/jacob/waveview/MainActivity.java
+++ app/src/main/java/com/jacob/waveview/MainActivity.java
@@ -2,14 +2,34 @@ package com.jacob.waveview;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 
 public class MainActivity extends FragmentActivity {
+    private WaterWaveView mWaterWave;
+    int progress = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWaterWave = (WaterWaveView) findViewById(R.id.waterWaveView);
+    }
+
+    public void start(View view) {
+        mWaterWave.startWave();
+    }
+
+    public void stop(View view) {
+        mWaterWave.stopWave();
+    }
+
+    public void progressUp(View view) {
+        mWaterWave.setProgress((progress++));
+    }
+
+    public void progressDown(View view) {
+        mWaterWave.setProgress((progress--));
     }
 
 }
