018240c59346d286e81b70f353d2d2a1410eb2e7
王佳 <wangjia@cvte.cn>
2015-03-30 10:55:31 +0800

    init Projects

---
+++ app/src/main/java/com/jacob/waveview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jacob.waveview;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
